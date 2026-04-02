92c961f5a70a0ade0f1d15c888ece570fc01a7b4
Pasha <p.n.soldatenko@gmail.com>
2026-04-02 13:06:28 +0300

    edit movement

--- src/main.cpp
+++ src/main.cpp
@@ -1099,9 +1099,60 @@ void turn_right(unsigned int step) {
 }
 
 void step_forward(unsigned int step) {
-  for (unsigned int i = 0; i < step; i++) {
-    // Реализация шага вперёд
-    delay(500);
+  if (!servosInitialized) return;
+  move_speed = leg_move_speed;
+  while (step-- > 0) {
+    if (site_now[2][1] == y_start) {
+      //leg 2&1 move
+      set_site(2, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+      set_site(2, x_default + x_offset, y_start + 2 * y_step, z_up);
+      wait_all_reach();
+      set_site(2, x_default + x_offset, y_start + 2 * y_step, z_default);
+      wait_all_reach();
+
+      move_speed = body_move_speed;
+
+      set_site(0, x_default + x_offset, y_start, z_default);
+      set_site(1, x_default + x_offset, y_start + 2 * y_step, z_default);
+      set_site(2, x_default - x_offset, y_start + y_step, z_default);
+      set_site(3, x_default - x_offset, y_start + y_step, z_default);
+      wait_all_reach();
+
+      move_speed = leg_move_speed;
+
+      set_site(1, x_default + x_offset, y_start + 2 * y_step, z_up);
+      wait_all_reach();
+      set_site(1, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+      set_site(1, x_default + x_offset, y_start, z_default);
+      wait_all_reach();
+    } else {
+      //leg 0&3 move
+      set_site(0, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+      set_site(0, x_default + x_offset, y_start + 2 * y_step, z_up);
+      wait_all_reach();
+      set_site(0, x_default + x_offset, y_start + 2 * y_step, z_default);
+      wait_all_reach();
+
+      move_speed = body_move_speed;
+
+      set_site(0, x_default - x_offset, y_start + y_step, z_default);
+      set_site(1, x_default - x_offset, y_start + y_step, z_default);
+      set_site(2, x_default + x_offset, y_start, z_default);
+      set_site(3, x_default + x_offset, y_start + 2 * y_step, z_default);
+      wait_all_reach();
+
+      move_speed = leg_move_speed;
+
+      set_site(3, x_default + x_offset, y_start + 2 * y_step, z_up);
+      wait_all_reach();
+      set_site(3, x_default + x_offset, y_start, z_up);
+      wait_all_reach();
+      set_site(3, x_default + x_offset, y_start, z_default);
+      wait_all_reach();
+    }
   }
 }
 
